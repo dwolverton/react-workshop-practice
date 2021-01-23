e2fe4726e86b8a4de8249fb9dcd95b9b3c5dbaa6
David Wolverton <dwolvert@gmail.com>
2021-01-23 13:11:28 -0500

    Added Votes Component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Exercise1 from "./Exercise1/Exercise1";
 import Exercise2 from "./Exercise2/Exercise2";
 import Exercise3 from "./Exercise3/Exercise3";
+import Exercise4 from "./Exercise4/Exercise4";
 import Exercise5 from "./Exercise5/Exercise5";
 import Exercise6 from "./Exercise6/Exercise6";
 import Exercise7 from "./Exercise7/Exercise7";
@@ -10,12 +11,13 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <Exercise1 />
+      {/* <Exercise1 />
       <Exercise2 />
-      <Exercise3 />
-      <Exercise5 />
+      <Exercise3 /> */}
+      <Exercise4 />
+      {/* <Exercise5 />
       <Exercise6 />
-      <Exercise7 />
+      <Exercise7 /> */}
     </div>
   );
 }
---
+++ src/Exercise4/Exercise4.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Votes from "./Votes";
+
+function Exercise4() {
+  return (
+    <section className="exercise">
+      <Votes />
+    </section>
+  );
+}
+
+export default Exercise4;
